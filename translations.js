b4c60e11d243b325ae6f137266f95a8651efad48
designos <112093794+designos@users.noreply.github.com>
2023-10-25 17:33:59 +0300

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,151 @@
+const translations = {
+    en: {
+        activity: "Activity",
+        vision: "Vision",
+        ourPartners: "Our Partners",
+        whyUs: "Why us",
+        contactUs: "Contact us",
+        english: "English",
+        arabic: "Arabic",
+        establishment: "Year Founded",
+        establishmentDesc: "The company was founded in 2013",
+        industry: "Industry",
+        industryDesc: "Bloomin Food and Beverage Trading Company LLC operates. In the food and beverage sector, which includes a wide range of products and services related to the food and beverage industry",
+        globalPresence: "Global presence",
+        globalPresenceDesc: "Bloomin Food and Beverage Trading Company LLC is considered a... Part of a group of companies. It has a presence in various locations around the world and may have branches, subsidiaries, or partners in different regions",
+        aboutCompany: "Company overview",
+        aboutCompanyDesc: "A limited liability company specializing in food and beverage trading is one of the leading companies in this sector. The company was established with the aim of providing high quality products and value-added services in the field of food and beverages",
+        ProductsAndServices: "Products and services",
+        ProductsAndServicesDesc: "The company offers a wide range of food and beverage products. This can include soft drinks, bottled water, raw materials for the food industry, frozen products, and more. In addition, the company may provide consulting services to restaurants, cafes and food establishments",
+        QualityAndCommitment: "Quality and commitment",
+        QualityAndCommitmentDesc: "This company intends to provide high quality products and adheres to the highest standards of quality and food safety. Suppliers are carefully selected and all products go through rigorous inspection processes.",
+        vision: "Vision",
+        visionDesc: "The company's vision is to become a leading supplier in the food and beverage industry, with a focus on quality and sustainability",
+        ClientsAndPartners: "Clients and partners",
+        ClientsAndPartnersDesc: "Bloomin customers are an essential partner in its success. Customer needs are met and a customized solution is provided. The company can also work with reliable suppliers and strategic partners",
+        SustainabilityAndEnvironment: "Sustainability and environment",
+        SustainabilityAndEnvironmentDesc: "The company is committed to its social and environmental responsibility. You may adopt sustainability initiatives, such as reducing plastic and moving towards eco-friendly products",
+        WhyWeAreTheBest: "Why we are the best",
+        wideSelection: "Wide selection",
+        wideSelectionDesc: "We offer a variety of food and beverage items that meet diverse requirements. Whether you are looking for fresh produce, raw materials for the food industry, or premium beverages, we have what you need.",
+        UnmatchedQuality: "Unmatched quality",
+        UnmatchedQualityDesc: "We are keen to provide high quality products that exceed customer expectations. We always aspire to achieve the highest standards of quality and food safety",
+        OutstandingCustomerService: "Outstanding customer service",
+        OutstandingCustomerServiceDesc: "We care about our customers' satisfaction and always strive to provide comprehensive support and a quick response to your inquiries and needs",
+        CommitmentToSustainability: "Commitment to sustainability",
+        CommitmentToSustainabilityDesc: "We are proud to contribute to environmental conservation and sustainability. We are committed to implementing environmentally friendly and sustainable practices in all aspects of our business",
+        ourPartners: "Our partners",
+        ourPartnersDesc: "We believe that strength comes from cooperation. Our partners are an integral part of our success. We cherish our partnerships and encourage interaction and exchange of ideas. Together, we effectively achieve our goals and build a bright future",
+        conclusion: "Conclusion",
+        conclusionDesc: "In the food and beverage business, Bloomin' is essential in meeting the community's food and beverage needs. This company provides products and services that meet high standards of quality and safety, and seeks to achieve success through a commitment to sustainability and customer satisfaction.",
+        contactUs: "Contact us",
+        footerDesc: "A limited liability company specializing in food and beverage trading is one of the leading companies in this sector. The company was established with the aim of providing high quality products and value-added services in the field of food and beverages",
+        copyRights: "Copyright © BLOMN - All rights reserved - 2023",
+    },
+    ar: {
+        activity: "النشاط",
+        vision: "الرؤية",
+        ourPartners: "شركائنا",
+        whyUs: "لماذا نحن الأفضل",
+        contactUs: "تواصل معنا",
+        english: "الإنجليزية",
+        arabic: "العربية",
+        establishment: "سنة التأسيس",
+        establishmentDesc: "تأسست الشركة في عام 2013",
+        industry: "الصناعة",
+        industryDesc: "تعمل شركة بلومن لتجارة الأغذية والمشروبات ذ.م.م. في قطاع الأغذية والمشروبات، والذي يضم مجموعة واسعة من المنتجات والخدمات المتعلقة بصناعة الطعام والمشروبات",
+        globalPresenceDesc: "تعتبر شركة بلومن لتجارة الأغذية والمشروبات ذ.ذ.م. جزءاً من مجموعة من الشركات. تمتلك وجوداً في مواقع مختلفة حول العالم قد تكون لديها فروع أو شركات تابعة أو شركاء في مناطق مختلفة",
+        globalPresence: "الوجود العالمي",
+        aboutCompany: "نبذة عن الشركة",
+        aboutCompanyDesc: "شركة ذات مسؤولية محدودة متخصصة في تجارة المواد الغذائية والمشروبات هي إحدى الشركات الرائدة في هذا القطاع. تأسست الشركة بهدف توفير منتجات عالية الجودة وخدمات ذات قيمة مضافة في مجال الغذاء والمشروبات",
+        ProductsAndServices: "منتجات وخدمات",
+        ProductsAndServicesDesc: "تقدم الشركة مجموعة واسعة من المنتجات الغذائية والمشروبات. ذلك يمكن أن يشمل المشروبات الغازية، المياه المعبأة، المواد الخام للصناعة الغذائية، المنتجات المجمدة، والمزيد. بالإضافة إلى ذلك، قد تقدم الشركة خدمات استشارية للمطاعم والمقاهي والمؤسسات الغذائية",
+        QualityAndCommitment: "الجودة والالتزام",
+        QualityAndCommitmentDesc: "تعتزم هذه الشركة بتقديم منتجات ذات جودة عالية وتلتزم بأعلى معايير الجودة والسلامة الغذائية يتم اختيار الموردين بعناية وتمر جميع المنتجات بعمليات فحص دقيقة",
+        vision: "الــرؤية",
+        visionDesc: "رؤية الشركة هي أن تصبح موردًا رائدًا في صناعة المواد الغذائية والمشروبات، مع التركيز على الجودة والاستدامة",
+        ClientsAndPartners: "العملاء والشركاء",
+        ClientsAndPartnersDesc: "تعتبر العملاء بلومن شريكًا أساسيًا في نجاحها. يتم تلبية احتياجات العملاء وتقديم حلاً مخصصًا لهم. كما يمكن أن تعمل الشركة مع موردين موثوقين وشركاء استراتيجيين",
+        SustainabilityAndEnvironment: "الاستدامة والبيئة",
+        SustainabilityAndEnvironmentDesc: "تلتزم الشركة بمسؤوليتها الاجتماعية والبيئية. قد تتبنى مبادرات استدامة، مثل تقليل البلاستيك والتوجه نحو منتجات صديقة للبيئة",
+        WhyWeAreTheBest: "لماذا نحن الأفضل",
+        wideSelection: "تشكيلة واسعة",
+        wideSelectionDesc: "نقدم مجموعة متنوعة من المواد الغذائية والمشروبات التي تلبي متطلبات متنوعة. سواء كنتم تبحثون عن منتجات طازجة، أو مواد خام لصناعة الأغذية، أو مشروبات فاخرة، ستجدون لدينا ما تحتاجون إليه",
+        UnmatchedQuality: "جودة لا تضاهي",
+        UnmatchedQualityDesc: "نحرص على تقديم منتجات ذات جودة عالية تفوق توقعات العملاء. نطمح دائمًا لتحقيق أقصى معايير الجودة والسلامة الغذائية",
+        OutstandingCustomerService: "خدمة العملاء المتميزة",
+        OutstandingCustomerServiceDesc: "نهتم برضا عملائنا ونسعى دائمًا لتقديم دعم شامل واستجابة سريعة لاستفساراتكم واحتياجاتكم",
+        CommitmentToSustainability: "خدمة العملاء المتميزة",
+        CommitmentToSustainabilityDesc: "نفخر بالمساهمة في الحفاظ على البيئة والاستدامة. نلتزم بتطبيق ممارسات صديقة للبيئة والاستدامة في جميع جوانب أعمالنا",
+        ourPartners: "شركائنا",
+        ourPartnersDesc: "نحن نؤمن بأن القوة تأتي من التعاون. شركاؤنا هم جزء لا يتجزأ من نجاحنا. نعتز بشراكاتنا ونشجع على التفاعل وتبادل الأفكار. سويا، نحقق أهدافنا بفعالية ونبني مستقبلًا مشرقًا",
+        conclusion: "ختاماً",
+        conclusionDesc: "تعتبر بلومن في مجال تجارة المواد الغذائية والمشروبات أساسية في تلبية احتياجات المجتمع من الطعام والشراب. توفر هذه الشركة منتجات وخدمات تلبي المعايير العالية للجودة والسلامة، وتسعى إلى تحقيق النجاح من خلال الالتزام بالاستدامة  ورضا العملاء",
+        contactUs: "تواصل معنا",
+        footerDesc: "شركة ذات مسؤولية محدودة متخصصة في تجارة المواد الغذائية والمشروبات هي إحدى الشركات الرائدة في هذا القطاع. تأسست الشركة بهدف توفير منتجات عالية الجودة وخدمات ذات قيمة مضافة في مجال الغذاء والمشروبات",
+        copyRights: "حقوق النشر © جميع الحقوق محفوظة لدى بلومن - 2023",
+    }
+};
+
+const languageSelector = document.querySelector("select")
+
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value)
+    localStorage.setItem("language", event.target.value)
+    if (event.target.value === 'ar') {
+        col2.style.textAlign = "right";
+        ProductsAndServices.style.textAlign = "right";
+        ProductsAndServicesDesc.style.textAlign = "right";
+        QualityAndCommitment.style.textAlign = "right";
+        QualityAndCommitmentDesc.style.textAlign = "right";
+        footerDesc.style.textAlign = "right";
+    } else {
+        col2.style.textAlign = "left";
+        ProductsAndServices.style.textAlign = "left";
+        ProductsAndServicesDesc.style.textAlign = "left";
+        QualityAndCommitment.style.textAlign = "left";
+        QualityAndCommitmentDesc.style.textAlign = "left";
+        footerDesc.style.textAlign = "left";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    setLanguage(localStorage.getItem("language"));
+});
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-i18n]")
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute('data-i18n')
+        element.textContent = translations[language][translationKey]
+    });
+    document.dir = language === 'ar' ? 'rtl' : 'ltr';
+};
+
+landingPagePhotoOne = document.querySelector("#landing-page-photo-1");
+landingPagePhotoTwo = document.querySelector("#landing-page-photo-2");
+landingPagePhotoThree = document.querySelector("#landing-page-photo-3");
+ProductsAndServices = document.querySelector(".ProductsAndServices");
+ProductsAndServicesDesc = document.querySelector(".ProductsAndServicesDesc");
+QualityAndCommitment = document.querySelector(".QualityAndCommitment");
+QualityAndCommitmentDesc = document.querySelector(".QualityAndCommitmentDesc");
+footerDesc = document.querySelector("#footerDesc");
+col2 = document.getElementById("col2");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
